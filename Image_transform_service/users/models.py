94ec077b86590ea321bfc47629ae885a7fe94d32
moeid05 <ostadsisq@gmail.com>
2025-08-14 13:51:08 +0330

    users added

---
+++ Image_transform_service/users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser,PermissionsMixin
+from django.db import models
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser,PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
---
+++ Image_transform_service/users/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+from django.contrib.auth import get_user_model
+User  = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = CustomUser
+        fields = ['username','email','password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
